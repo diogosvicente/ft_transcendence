5bd0feba21b92279048ab40030ef4cb5ae75f90f
almelo <algacyroalga@gmail.com>
2024-08-29 15:12:51 -0300

    Initial canvas setup

--- pong-client/src/App.jsx
+++ pong-client/src/App.jsx
@@ -1,7 +1,20 @@
-import './App.css'
+import { useEffect, useRef } from 'react';
+//import './App.css'
 
 function Canvas() {
-    return <h1>Hello, Vite!</h1>
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }, []);
+
+    return (
+        <canvas ref={canvasRef} width={800} height={590}></canvas>
+    );
 }
 
 function App() {
