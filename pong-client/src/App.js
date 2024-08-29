65231b4ac77cea0605878ae85789e6bffa93d752
almelo <algacyroalga@gmail.com>
2024-08-29 14:39:37 -0300

    Recreate the project using Vite's react template

--- pong-client/src/App.js
+++
@@ -1,7 +0,0 @@
-function Hello({ name }) {
-    return <h1>Hello, {name}!</h1>;
-}
-
-export default function App() {
-    return <Hello name={"Transcendence"} />
-}
---
+++ pong-client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function Canvas() {
+    return <h1>Hello, Vite!</h1>
+}
+
+function App() {
+
+    return (
+        <>
+            <Canvas />
+        </>
+    );
+}
+
+export default App
