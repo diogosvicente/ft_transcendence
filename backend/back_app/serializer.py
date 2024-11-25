3511c8c5bad818b41cef972eedb050ab8c9d1f18
Diogo <diogosk84ever@hotmail.com>
2024-11-25 18:06:44 -0300

    feat: restarting backend configuration

--- backend/back_app/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from back_app.models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'password', 'display_name', 'avatar', 'created_at', 'updated_at', 'is_deleted']
-        read_only_fields = ['id', 'created_at', 'updated_at']
