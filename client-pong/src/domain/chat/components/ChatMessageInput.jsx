e43df3dce4f17a695e4cef7fa6186d94b63739e9
algacyr <algacyroalga@gmail.com>
2024-12-10 22:17:38 -0300

    handle chat connection and live messages

--- client-pong/src/domain/chat/components/ChatMessageInput.jsx
+++ client-pong/src/domain/chat/components/ChatMessageInput.jsx
@@ -1,17 +1,31 @@
-export function ChatMessageInput() {
+import { useRef } from "react";
+
+export function ChatMessageInput({ onChatSendButtonClick, onMessageChange, message }) {
+  const chatSendButtonRef = useRef(null);
+
+  // Manually triggers click event for send button
+  // when Enter key is pressed
+  const handleKeyUp = (e) => {
+    if (e.key === 'Enter') {
+      chatSendButtonRef.current.click();
+    }
+  }
+
   return (
     <>
       <input
         type="text"
-        id="chat-message-input"
-        placeholder="Send a message"
+        value={message} placeholder="Send a message"
+        onChange={(e) => onMessageChange(e.target.value)}
         size="100"
+        onKeyUp={handleKeyUp}
       />
       <br />
       <input
-        id="chat-message-submit"
         type="button"
         value="Send"
+        onClick={onChatSendButtonClick}
+        ref={chatSendButtonRef}
       />
     </>
   );
--- client-pong/src/domain/chat/containers/Chat.jsx
+++ client-pong/src/domain/chat/containers/Chat.jsx
@@ -1,28 +1,51 @@
 import { useParams } from 'react-router-dom';
 import { ChatLog } from '../components/ChatLog.jsx'
 import { ChatMessageInput } from '../components/ChatMessageInput.jsx'
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+/* TODO: Scroll the chat log to the last message */
+/* TODO: Add more socket event handlers */
 
 export default function Chat() {
   const { roomName } = useParams();
 
+  const [message, setMessage] = useState('');
+  const [chatLogMessages, setChatLogMessages] = useState([]);
+
+  const chatSocketRef = useRef(null);
+
   const WS_SERVER_HOST = 'localhost:8000';
   const socketUrl = `ws://${WS_SERVER_HOST}/ws/chat/${roomName}/`;
 
+  // Start WS server connection on component mounting
   useEffect(() => {
-    const chatSocket = new WebSocket(socketUrl);
+    if (chatSocketRef.current) { return; } // Prevent react's strict mode re-render
 
-    chatSocket.onopen = () => {
-      console.log('CONNECTION ESTABLISHED');
+    const socket = new WebSocket(socketUrl);
+
+    socket.onmessage = function(e) {
+      const data = JSON.parse(e.data);
+      setChatLogMessages(prevLog => [...prevLog, data.message]);
     };
 
+    chatSocketRef.current = socket;
   }, []);
+
+  const handleChatSendButtonClick = () => {
+    chatSocketRef.current.send(JSON.stringify({
+      'message': message
+    }));
+    setMessage('');
+  };
 
   return (
     <>
-      <h2>Chat Room: {roomName}</h2>
-      <ChatLog /><br />
-      <ChatMessageInput />
+      <ChatLog messages={chatLogMessages} /><br />
+      <ChatMessageInput
+        onChatSendButtonClick={handleChatSendButtonClick}
+        onMessageChange={setMessage}
+        message={message}
+      />
     </>
   );
 }
