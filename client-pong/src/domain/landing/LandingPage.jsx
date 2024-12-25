3c3640de7a74dff85d71dbae201529e424d213dd
Diogo <diogosk84ever@hotmail.com>
2024-12-25 20:26:07 -0300

    fix: 2FA fixed and added some icons at landing page

--- client-pong/src/domain/landing/LandingPage.jsx
+++ client-pong/src/domain/landing/LandingPage.jsx
@@ -3,11 +3,16 @@ import { Button, Stack, Form, Container, Alert, Tabs, Tab } from "react-bootstra
 import LoadingModal from "./LoadingModal";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import API_BASE_URL from "../../assets/config/config.js"; // Importando a URL da API
-import brazilFlag from "../../assets/icons/brazil-flag-round-circle-icon.svg"; // Imagem do Brasil
+import API_BASE_URL from "../../assets/config/config.js";
+import brazilFlag from "../../assets/icons/brazil-flag-round-circle-icon.svg";
 import spainFlag from "../../assets/icons/spain-country-flag-round-icon.svg";
 import ukFlag from "../../assets/icons/uk-flag-round-circle-icon.svg";
-import "../../assets/styles/landingPage.css"; // Estilo personalizado (adicione o arquivo CSS)
+import "../../assets/styles/landingPage.css";
+
+// ícones
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash, faSignInAlt, faUserPlus, faKey } from "@fortawesome/free-solid-svg-icons";
+import { faTableTennis } from "@fortawesome/free-solid-svg-icons";
 
 const LoginAndRegisterForm = () => {
   const { t, i18n } = useTranslation();
@@ -30,6 +35,7 @@ const LoginAndRegisterForm = () => {
     specialChar: false,
   });
   const [activeTab, setActiveTab] = useState("login");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -41,6 +47,10 @@ const LoginAndRegisterForm = () => {
 
   const handleLanguageChange = (language) => {
     i18n.changeLanguage(language);
+  };
+
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword);
   };
 
   const handleClose = () => setShowLoading(false);
@@ -75,52 +85,6 @@ const LoginAndRegisterForm = () => {
   const isPasswordValid = () => {
     const { minLength, uppercase, lowercase, number, specialChar } = passwordValidation;
     return minLength && uppercase && lowercase && number && specialChar;
-  };
-
-  const handleRegister = async (event) => {
-    event.preventDefault();
-    setValidated(true);
-    setErrorMessage("");
-    setSuccessMessage("");
-
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
-      setErrorMessage(t("error_invalid_email"));
-      return;
-    }
-
-    if (!isPasswordValid()) {
-      setErrorMessage(t("error_password_requirements"));
-      return;
-    }
-
-    handleShow();
-
-    const formDataToSend = new FormData();
-    formDataToSend.append("email", formData.email);
-    formDataToSend.append("password", formData.password);
-    if (formData.avatar) {
-      formDataToSend.append("avatar", formData.avatar);
-    }
-
-    try {
-      const response = await fetch(`${API_BASE_URL}/api/user-management/register/`, {
-        method: "POST",
-        body: formDataToSend,
-      });
-
-      const data = await response.json();
-
-      if (response.ok) {
-        setSuccessMessage(t("success_register"));
-        setActiveTab("login");
-      } else {
-        setErrorMessage(data.email ? data.email[0] : t("error_unknown"));
-      }
-    } catch (error) {
-      setErrorMessage(t("error_connection"));
-    } finally {
-      handleClose();
-    }
   };
 
   const handleLogin = async (event) => {
@@ -161,6 +125,58 @@ const LoginAndRegisterForm = () => {
       }
     } catch (error) {
       setErrorMessage(t("error_connection"));
+    } finally {
+      handleClose();
+    }
+  };
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    setValidated(true);
+    setErrorMessage("");
+    setSuccessMessage("");
+
+    // Validação de e-mail
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(formData.email)) {
+      setErrorMessage("Por favor, insira um e-mail válido.");
+      return;
+    }
+
+    // Validação de senha forte
+    if (!isPasswordValid()) {
+      setErrorMessage("Por favor, preencha os requisitos de senha antes de continuar.");
+      return;
+    }
+
+    handleShow();
+
+    const formDataToSend = new FormData();
+    formDataToSend.append("email", formData.email);
+    formDataToSend.append("password", formData.password);
+    if (formData.avatar) {
+      formDataToSend.append("avatar", formData.avatar);
+    }
+
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/api/user-management/register/`,
+        {
+          method: "POST",
+          body: formDataToSend,
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setSuccessMessage("Usuário cadastrado com sucesso!");
+        setActiveTab("login");
+      } else {
+        setErrorMessage(data.email ? data.email[0] : "Erro desconhecido.");
+      }
+    } catch (error) {
+      setErrorMessage("Erro ao conectar ao servidor.");
     } finally {
       handleClose();
     }
@@ -226,197 +242,242 @@ const LoginAndRegisterForm = () => {
 
   return (
     <div className="vh-100 d-flex flex-column justify-content-center align-items-center">
-  {/* Language Selector */}
-  <div className="language-selector position-absolute top-0 end-0 mt-3 me-3">
-    <div className="d-flex gap-3">
-      <div
-        className="language-card"
-        onClick={() => handleLanguageChange("pt_BR")}
-      >
-        <img
-          src={brazilFlag}
-          alt="Português (Brasil)"
-          className="language-flag"
-        />
-        <span className="language-text">PT-BR</span>
-      </div>
-      <div
-        className="language-card"
-        onClick={() => handleLanguageChange("en")}
-      >
-        <img
-          src={ukFlag}
-          alt="English"
-          className="language-flag"
-        />
-        <span className="language-text">EN</span>
-      </div>
-      <div
-        className="language-card"
-        onClick={() => handleLanguageChange("es")}
-      >
-        <img
-          src={spainFlag}
-          alt="Español"
-          className="language-flag"
-        />
-        <span className="language-text">ES</span>
-      </div>
-    </div>
-  </div>
-
-  <h1 className="mb-4">{t("app_title")}</h1>
-  <Container className="col-lg-4 border rounded p-4 mx-auto">
-    <Tabs
-      activeKey={activeTab}
-      onSelect={(k) => setActiveTab(k)}
-      className="mb-3"
-    >
-      <Tab eventKey="login" title={t("login")}>
-        <Form noValidate validated={validated}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>{t("email_label")}</Form.Label>
-            <Form.Control
-              required
-              type="text"
-              placeholder={t("email_placeholder")}
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              {t("required_field")}
-            </Form.Control.Feedback>
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>{t("password_label")}</Form.Label>
-            <Form.Control
-              required
-              type="password"
-              placeholder={t("password_placeholder")}
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              {t("required_field")}
-            </Form.Control.Feedback>
-          </Form.Group>
-
-          {is2FARequired && (
-            <Form.Group className="mb-3" controlId="form2FACode">
-              <Form.Label>{t("2fa_code_label")}</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder={t("code_2fa_placeholder")}
-                name="code"
-                value={formData.code}
-                onChange={handleChange}
-              />
-              <Form.Control.Feedback type="invalid">
-                {t("required_field")}
-              </Form.Control.Feedback>
-            </Form.Group>
-          )}
-
-          {successMessage && <Alert variant="success">{successMessage}</Alert>}
-          {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
-
-          <Stack
-            direction="horizontal"
-            gap={4}
-            className="py-4 d-flex justify-content-center"
+      {/* Language Selector */}
+      <div className="language-selector position-absolute top-0 end-0 mt-3 me-3">
+        <div className="d-flex gap-3">
+          <div
+            className="language-card"
+            onClick={() => handleLanguageChange("pt_BR")}
           >
-            {is2FARequired ? (
-              <Button
-                variant="dark"
-                className="w-50"
-                onClick={handleValidate2FA}
-              >
-                {t("validate_code")}
-              </Button>
-            ) : (
-              <Button
-                variant="outline-secondary"
-                className="w-50"
-                onClick={handleLogin}
-              >
+            <img
+              src={brazilFlag}
+              alt="Português (Brasil)"
+              className="language-flag"
+            />
+            <span className="language-text">PT-BR</span>
+          </div>
+          <div
+            className="language-card"
+            onClick={() => handleLanguageChange("en")}
+          >
+            <img
+              src={ukFlag}
+              alt="English"
+              className="language-flag"
+            />
+            <span className="language-text">EN</span>
+          </div>
+          <div
+            className="language-card"
+            onClick={() => handleLanguageChange("es")}
+          >
+            <img
+              src={spainFlag}
+              alt="Español"
+              className="language-flag"
+            />
+            <span className="language-text">ES</span>
+          </div>
+        </div>
+      </div>
+
+      <h1 className="mb-4 d-flex align-items-center justify-content-center">
+        <FontAwesomeIcon icon={faTableTennis} className="me-3" />
+        {t("app_title")}
+      </h1>
+      <Container className="col-lg-4 border rounded p-4 mx-auto">
+        <Tabs
+          activeKey={activeTab}
+          onSelect={(k) => setActiveTab(k)}
+          className="mb-3 custom-tabs"
+        >
+          <Tab
+            eventKey="login"
+            title={
+              <span>
+                <FontAwesomeIcon icon={faSignInAlt} className="me-2" />
                 {t("login")}
-              </Button>
-            )}
-          </Stack>
-        </Form>
-      </Tab>
-
-      <Tab eventKey="register" title={t("register")}>
-        <Form noValidate validated={validated}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>{t("email_label")}</Form.Label>
-            <Form.Control
-              required
-              type="text"
-              placeholder={t("email_placeholder")}
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              {t("required_field")}
-            </Form.Control.Feedback>
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>{t("password_label")}</Form.Label>
-            <Form.Control
-              required
-              type="password"
-              placeholder={t("password_placeholder")}
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              {t("required_field")}
-            </Form.Control.Feedback>
-            {renderPasswordRequirements()}
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formAvatar">
-            <Form.Label>{t("avatar_label")}</Form.Label>
-            <Form.Control
-              type="file"
-              name="avatar"
-              accept=".jpg,.png"
-              onChange={handleChange}
-            />
-            <Form.Text className="text-muted">{t("avatar_info")}</Form.Text>
-          </Form.Group>
-
-          {successMessage && <Alert variant="success">{successMessage}</Alert>}
-          {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
-
-          <Stack
-            direction="horizontal"
-            gap={4}
-            className="py-4 d-flex justify-content-center"
+              </span>
+            }
           >
-            <Button
-              variant="dark"
-              className="w-50"
-              onClick={handleRegister}
-            >
-              {t("register")}
-            </Button>
-          </Stack>
-        </Form>
-      </Tab>
-    </Tabs>
-    <LoadingModal showLoading={showLoading} handleClose={handleClose} />
-  </Container>
-</div>
+            <Form noValidate validated={validated}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>{t("email_label")}</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder={t("email_placeholder")}
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {t("required_field")}
+                </Form.Control.Feedback>
+              </Form.Group>
 
+              <Form.Group
+                className="mb-3 position-relative"
+                controlId="formBasicPassword"
+              >
+                <Form.Label>{t("password_label")}</Form.Label>
+                <div className="d-flex">
+                  <Form.Control
+                    required
+                    type={showPassword ? "text" : "password"}
+                    placeholder={t("password_placeholder")}
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                  />
+                  <Button
+                    variant="outline-secondary"
+                    className="ms-2"
+                    onClick={handlePasswordToggle}
+                  >
+                    <FontAwesomeIcon
+                      icon={showPassword ? faEyeSlash : faEye}
+                    />
+                  </Button>
+                </div>
+                <Form.Control.Feedback type="invalid">
+                  {t("required_field")}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              {is2FARequired && (
+                <Form.Group className="mb-3" controlId="form2FACode">
+                  <Form.Label>{t("2fa_code_label")}</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder={t("code_2fa_placeholder")}
+                    name="code"
+                    value={formData.code}
+                    onChange={handleChange}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {t("required_field")}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              )}
+
+              {successMessage && (
+                <Alert variant="success">{successMessage}</Alert>
+              )}
+              {errorMessage && (
+                <Alert variant="danger">{errorMessage}</Alert>
+              )}
+
+              <Stack
+                direction="horizontal"
+                gap={4}
+                className="py-4 d-flex justify-content-center"
+              >
+                {is2FARequired ? (
+                  <Button
+                    variant="dark"
+                    className="w-50"
+                    onClick={handleValidate2FA}
+                  >
+                    <FontAwesomeIcon icon={faKey} className="me-2" />
+                    {t("validate_code")}
+                  </Button>
+                ) : (
+                  <Button
+                    variant="outline-secondary"
+                    className="w-50"
+                    onClick={handleLogin}
+                  >
+                    <FontAwesomeIcon icon={faSignInAlt} className="me-2" />
+                    {t("login")}
+                  </Button>
+                )}
+              </Stack>
+            </Form>
+          </Tab>
+
+          <Tab
+            eventKey="register"
+            title={
+              <span>
+                <FontAwesomeIcon icon={faUserPlus} className="me-2" />
+                {t("register")}
+              </span>
+            }
+          >
+            <Form noValidate validated={validated}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>{t("email_label")}</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder={t("email_placeholder")}
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {t("required_field")}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>{t("password_label")}</Form.Label>
+                <Form.Control
+                  required
+                  type="password"
+                  placeholder={t("password_placeholder")}
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {t("required_field")}
+                </Form.Control.Feedback>
+                {renderPasswordRequirements()}
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formAvatar">
+                <Form.Label>{t("avatar_label")}</Form.Label>
+                <Form.Control
+                  type="file"
+                  name="avatar"
+                  accept=".jpg,.png"
+                  onChange={handleChange}
+                />
+                <Form.Text className="text-muted">
+                  {t("avatar_info")}
+                </Form.Text>
+              </Form.Group>
+
+              {successMessage && (
+                <Alert variant="success">{successMessage}</Alert>
+              )}
+              {errorMessage && (
+                <Alert variant="danger">{errorMessage}</Alert>
+              )}
+
+              <Stack
+                direction="horizontal"
+                gap={4}
+                className="py-4 d-flex justify-content-center"
+              >
+                <Button
+                  variant="dark"
+                  className="w-50"
+                  onClick={handleRegister}
+                >
+                  <FontAwesomeIcon icon={faUserPlus} className="me-2" />
+                  {t("register")}
+                </Button>
+              </Stack>
+            </Form>
+          </Tab>
+        </Tabs>
+        <LoadingModal showLoading={showLoading} handleClose={handleClose} />
+      </Container>
+    </div>
   );
 };
 
--- client-pong/src/domain/template/Navbar.jsx
+++ client-pong/src/domain/template/Navbar.jsx
@@ -4,10 +4,14 @@ import { Navbar, Nav, Button, Container, Image } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import API_BASE_URL, { API_BASE_URL_NO_LANGUAGE } from "../../assets/config/config.js";
-import brazilFlag from "../../assets/icons/brazil-flag-round-circle-icon.svg"; // Imagem do Brasil
-import spainFlag from "../../assets/icons/spain-country-flag-round-icon.svg"; // Imagem da Espanha
-import ukFlag from "../../assets/icons/uk-flag-round-circle-icon.svg"; // Imagem do Reino Unido
-import "../../assets/styles/navbar.css"; // Estilo personalizado (adicione o arquivo CSS)
+import brazilFlag from "../../assets/icons/brazil-flag-round-circle-icon.svg";
+import spainFlag from "../../assets/icons/spain-country-flag-round-icon.svg";
+import ukFlag from "../../assets/icons/uk-flag-round-circle-icon.svg";
+import "../../assets/styles/navbar.css";
+
+// Ícone da raquete de tênis de mesa
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTableTennis } from "@fortawesome/free-solid-svg-icons";
 
 const CustomNavbar = () => {
   const navigate = useNavigate();
@@ -16,7 +20,7 @@ const CustomNavbar = () => {
 
   useEffect(() => {
     const email = localStorage.getItem("email");
-    const defaultAvatar = `${API_BASE_URL_NO_LANGUAGE}/media/avatars/default.png`; // URL completa da imagem padrão
+    const defaultAvatar = `${API_BASE_URL_NO_LANGUAGE}/media/avatars/default.png`;
 
     if (email) {
       fetch(`${API_BASE_URL}/api/user-management/avatar?email=${email}`)
@@ -51,98 +55,97 @@ const CustomNavbar = () => {
 
   return (
     <Navbar bg="light" expand="lg" className="navbar-custom">
-  <Container>
-    {/* Lado esquerdo: Avatar e nome "Pong" */}
-    <div className="d-flex align-items-center">
-      <Image
-        src={avatar}
-        alt="Avatar"
-        roundedCircle
-        width="40"
-        height="40"
-        className="me-2"
-      />
-      <Navbar.Brand href="/home" className="navbar-brand">
-        {t("navbar.app_title")}
-      </Navbar.Brand>
-    </div>
-
-    {/* Botão de hambúrguer para telas pequenas */}
-    <Navbar.Toggle
-      aria-controls="basic-navbar-nav"
-      className="custom-toggler"
-    >
-      <span className="toggler-icon"></span>
-      <span className="toggler-icon"></span>
-      <span className="toggler-icon"></span>
-    </Navbar.Toggle>
-
-    <Navbar.Collapse
-      id="basic-navbar-nav"
-      className="justify-content-end align-items-center custom-collapse"
-    >
-      <Nav className="me-3">
-        <NavLink to="/chat" className="nav-link">
-          {t("navbar.chat")}
-        </NavLink>
-        <NavLink to="/tournaments" className="nav-link">
-          {t("navbar.tournaments")}
-        </NavLink>
-        <NavLink to="/profile" className="nav-link">
-          {t("navbar.profile")}
-        </NavLink>
-        <NavLink to="/history" className="nav-link">
-          {t("navbar.history")}
-        </NavLink>
-        <NavLink to="/friends" className="nav-link">
-          {t("navbar.friends")}
-        </NavLink>
-      </Nav>
-      <div className="d-flex align-items-center">
-        <div className="language-selector d-flex gap-2">
-          <div
-            className="language-card"
-            onClick={() => handleLanguageChange("pt_BR")}
-          >
-            <img
-              src={brazilFlag}
-              alt="Português (Brasil)"
-              className="language-flag"
-            />
-            <span className="language-text">PT-BR</span>
-          </div>
-          <div
-            className="language-card"
-            onClick={() => handleLanguageChange("en")}
-          >
-            <img
-              src={ukFlag}
-              alt="English"
-              className="language-flag"
-            />
-            <span className="language-text">EN</span>
-          </div>
-          <div
-            className="language-card"
-            onClick={() => handleLanguageChange("es")}
-          >
-            <img
-              src={spainFlag}
-              alt="Español"
-              className="language-flag"
-            />
-            <span className="language-text">ES</span>
-          </div>
+      <Container>
+        {/* Lado esquerdo: Avatar e nome "Pong Game" com a raquete */}
+        <div className="d-flex align-items-center">
+          <Image
+            src={avatar}
+            alt="Avatar"
+            roundedCircle
+            width="40"
+            height="40"
+            className="me-2"
+          />
+          <Navbar.Brand href="/home" className="navbar-brand d-flex align-items-center">
+            <FontAwesomeIcon icon={faTableTennis} className="me-2" />
+            {t("navbar.app_title")}
+          </Navbar.Brand>
         </div>
-        <Button variant="outline-secondary" className="ms-3" onClick={handleLogout}>
-          {t("navbar.logout")}
-        </Button>
-      </div>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
 
+        {/* Botão de hambúrguer para telas pequenas */}
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="custom-toggler"
+        >
+          <span className="toggler-icon"></span>
+          <span className="toggler-icon"></span>
+          <span className="toggler-icon"></span>
+        </Navbar.Toggle>
 
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="justify-content-end align-items-center custom-collapse"
+        >
+          <Nav className="me-3">
+            <NavLink to="/chat" className="nav-link">
+              {t("navbar.chat")}
+            </NavLink>
+            <NavLink to="/tournaments" className="nav-link">
+              {t("navbar.tournaments")}
+            </NavLink>
+            <NavLink to="/profile" className="nav-link">
+              {t("navbar.profile")}
+            </NavLink>
+            <NavLink to="/history" className="nav-link">
+              {t("navbar.history")}
+            </NavLink>
+            <NavLink to="/friends" className="nav-link">
+              {t("navbar.friends")}
+            </NavLink>
+          </Nav>
+          <div className="d-flex align-items-center">
+            <div className="language-selector d-flex gap-2">
+              <div
+                className="language-card"
+                onClick={() => handleLanguageChange("pt_BR")}
+              >
+                <img
+                  src={brazilFlag}
+                  alt="Português (Brasil)"
+                  className="language-flag"
+                />
+                <span className="language-text">PT-BR</span>
+              </div>
+              <div
+                className="language-card"
+                onClick={() => handleLanguageChange("en")}
+              >
+                <img
+                  src={ukFlag}
+                  alt="English"
+                  className="language-flag"
+                />
+                <span className="language-text">EN</span>
+              </div>
+              <div
+                className="language-card"
+                onClick={() => handleLanguageChange("es")}
+              >
+                <img
+                  src={spainFlag}
+                  alt="Español"
+                  className="language-flag"
+                />
+                <span className="language-text">ES</span>
+              </div>
+            </div>
+            <Button variant="outline-secondary" className="ms-3" onClick={handleLogout}>
+              {t("navbar.logout")}
+            </Button>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
