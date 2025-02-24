b3ea729d5de1b311ca051852aee352808a46bc59
Diogo Vicente <diogosk84ever@hotmail.com>
2025-02-23 23:36:36 -0300

    fix users avatar at chatfriendlist

--- client-pong/src/assets/config/config.js
+++ client-pong/src/assets/config/config.js
@@ -10,11 +10,15 @@ export const API_BASE_URL = "https://192.168.1.138";
 export const DEFAULT_AVATAR = `/media/avatars/default.png`;
 
 export const getAvatar = (avatarPath) => {
-  if (!avatarPath) return DEFAULT_AVATAR;
+  if (!avatarPath || avatarPath === "/media/") {
+    return DEFAULT_AVATAR;
+  }
+
   return avatarPath.startsWith("/media/")
     ? ``
     : `/media/`;
 };
+
 
 export const getWsUrl = (path = "") => {
   return "wss://192.168.1.138" + path;  // e.g. /ws/chat/
